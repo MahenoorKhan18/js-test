026de9e6e15ab20ee11344bc6e88a7b85eb9da22
MahenoorKhan18 <khanmahenoor1818@gmail.com>
2025-03-03 20:42:56 +0000

    conversion

---
+++ 1_js/conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "33"
+
+// let valueInNumber = Number(score) //it will convert the vale in number
+// console.log(typeof score);
+
+// let score1 = "33abc"
+// let valueInNumber1 = Number(score1)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1); //NaN means not a number
+// console.log(valueInNumber);
+
+// "33" => if we conver the value string 33 will convert into number
+//"33abc" => it will not convert the output will be NaN
+// true will conver into 1 and false will conver into 0 ("" => false and "hi" => true)
+
+/* Boolean convertion */
+// let Isloggedin =1
+// let booleanIsloggedin = Boolean(Isloggedin)
+// console.log(booleanIsloggedin);
+
+/* String convertion */
+let isloggedOut = 34
+let stringisloggedOut = String(isloggedOut)
+console.log(typeof stringisloggedOut);
+
+
+
+
+
