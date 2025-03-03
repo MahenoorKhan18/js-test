9fcc3277d7a886b8c327441cb5209f9205c069f4
MahenoorKhan18 <khanmahenoor1818@gmail.com>
2025-03-03 20:00:18 +0000

    datatypes

--- 1_js/dataTypes.js
+++ 1_js/dataTypes.js
@@ -1,3 +1,18 @@
 "use strict"; // treat all JS code as newer verssion 
 
 /* alert(3+3) it will show error because it is new js*/
+
+console.log(3 + 3) 
+
+console.log("Mahenoor")
+
+
+//Data Types /
+let name = "Mahenoor" //string data type
+let age = 21  //integer data type
+let isloggedIn = true  // boolean data type
+//null  data type it is a stand alone value (it means empty value and we a sign it )
+// undefined if we write a veriable but doesnot give any value then it is undefined
+// symbol used for unique
+// object
+//console.log (typeof ) it is uused to find out the type of any variable
